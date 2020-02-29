108476459188b907d3792ee20d406b8562a17f0b
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-29 21:10:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,7 +12,7 @@ def directors_totals(nds)
     movie_index = 0
     
     while movie_index < nds[director_index].length do
-    totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
     movie_index += 1
   end
   
